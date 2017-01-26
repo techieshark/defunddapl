c25cefec74283d9f9dc561542866a5b8ea735e71
Peter W <techieshark@gmail.com>
2017-01-25 23:28:44 -0800

    build / config changes for Flow static type checking
    * note we had to update airbnb to allow importing flow types
    see: https://github.com/airbnb/javascript/issues/1054
    
    so I can do
    	import foo from ./bar
    	import type { fooT } from ./bar
    without complaints

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,33 @@
 module.exports = {
-    "extends": "airbnb",
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended",
+        "plugin:flowtype/recommended"
+    ],
+    "parser": "babel-eslint",
     "plugins": [
+        "flowtype",
         "react",
+        "react-native",
         "jsx-a11y",
         "import"
     ],
     "rules": {
         "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+        "react/display-name": 0,
         "no-console": 0,
         "quotes": 0,
+        "react/sort-comp": [1, {
+            order: [
+                'type-annotations',
+                'static-methods',
+                'lifecycle',
+                'everything-else',
+                'render',
+            ],
+        }],
+        // "react-native/no-unused-styles": 1,
+        // "react-native/no-inline-styles": 1,
+        // "react-native/no-color-literals": 1
     }
 };
--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 run:
 	react-native run-ios
 
+flow:
+	 npm run-script flow
+
 console:
 	react-native log-ios
 	# react-native log-android
